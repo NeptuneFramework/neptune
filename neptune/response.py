dcca0713734fa65c45fbb2e73d3c26c65ca46385
Yash Mehrotra <yashmehrotra95@gmail.com>
2016-03-01 07:04:23 +0530

    Change HTTPResponse API

--- neptune/response.py
+++ neptune/response.py
@@ -7,7 +7,7 @@ class HTTPResponse(object):
         self.response = self._generate_response(http_ver, status, headers, body)
 
     def _generate_response(self, http_version, status, headers, body):
-        base = "{0} {1}\r\n".format(http_ver, status)
+        base = "{0} {1}\r\n".format(http_version, status)
         for header in headers:
             base += "{0}: {1}\r\n".format(header, headers[header])
         # Also add custom headers (Server: Neptune)
